747fdd37676853788ff23d1e19c4589e38099b31
Vishal Singh <4848vishalsingh@gmail.com>
2025-09-05 20:04:10 +0530

    Remove all files for fresh start

--- backend/src/config/env.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const ENV = {
-  PORT: process.env.PORT || 5000,
-  MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/modexdb",
-  JWT_SECRET: process.env.JWT_SECRET || "supersecretkey",
-};
